f1fc1ecf133eaa24546ad0223b789c4e8de68133
Ritsudako96 <100332529+Ritsudako96@users.noreply.github.com>
2024-01-15 22:18:18 -0800

    Update survey.js

--- survey.js
+++ survey.js
@@ -5,7 +5,7 @@ form.addEventListener('submit', (e)=>{
 	e.preventDefault();
 	document.querySelector("#submit").value="Submitting...";
 	let data = new FormData(form);
-	fetch('https://script.google.com/macros/s/AKfycbyfWhEuYPJOQ1t10GWeTHTnq9G8NobsVXzGuJIKyVtoVXyq5nI6qAS454JJgnqe2-MZ/exec', {
+	fetch('https://script.google.com/macros/s/AKfycbyFHpyGmjChD7z1eE4vnroaeVuqpn6L4WuMt5jszloYCxNPgwXE2G5XGz5KkZexs7o5/exec', {
 		method:"POST",
 		body:data
 	})
